a0ae8cc4af26516ba71744c3b438dc3b117f747b
gil.platt <gplatt@xentity.com>
2022-03-13 21:06:02 -0600

    current state with mobile menu links

--- src/components/header-bar/header-bar.vue
+++ src/components/header-bar/header-bar.vue
@@ -15,7 +15,7 @@
             role="button"
             aria-labelledby="Set fonts and colors for visual assistance"
             ref="menu"
-            @click="isFocused = !isFocused"
+            @click="accessibilityMenuOpen = !accessibilityMenuOpen"
           >
             Accessibility
             <font-awesome-icon
@@ -26,7 +26,7 @@
             <div
               class="accessibility-menu"
               aria-labelledby="Dropdown menu"
-              v-show="isFocused"
+              v-show="accessibilityMenuOpen"
             >
               <a
                 role="button"
@@ -41,31 +41,67 @@
           </span>
         </div>
       </div>
-      <span class="site-links" aria-labelledby="Site links">
+      <div
+        id="mobile-styles"
+        aria-labelledby="A container to style the navigation menu for mobile users. Disregard this message if you are a desktop user."
+        @click="setMobileMenuOpen()"
+      >
+        <span class="site-links" aria-labelledby="Site links">
+          <a
+            :class="currentPage === item.page ? 'current-link' : 'site-link'"
+            v-for="item in headerItems"
+            :key="item.page"
+            @click="setCurrentPage(item.page)"
+          >
+            {{ item.page }}
+          </a>
+        </span>
+        <div class="donor-social-navigation">
+          <span class="social-media-links" aria-labelledby="Social media links">
+            <a
+              class="social-media-link"
+              v-for="item in socialMediaItems"
+              :key="item.brand"
+            >
+              <font-awesome-icon :icon="`fa-brands fa-${item.brand}`" />
+            </a>
+          </span>
+          <a class="donor-link" role="button">
+            <span
+              class="donor-button"
+              data-alttext="Donate"
+              aria-label="link to the donation page"
+            >
+              <span>Become a donor</span>
+            </span>
+          </a>
+        </div>
+      </div>
+      <div
+        id="mobile-menu"
+        v-show="mobileMenuOpen"
+        aria-placeholder="Mobile site menu"
+      >
         <a
-          :class="currentPage === item.page ? 'current-link' : 'site-link'"
+          :class="
+            currentPage === item.page
+              ? 'current-mobile-link'
+              : 'mobile-menu-link'
+          "
           v-for="item in headerItems"
           :key="item.page"
           @click="setCurrentPage(item.page)"
         >
           {{ item.page }}
         </a>
-      </span>
-      <div class="donor-social-navigation">
-        <span class="social-media-links" aria-labelledby="Social media links">
-          <a
-            class="social-media-link"
-            v-for="item in socialMediaItems"
-            :key="item.brand"
-          >
-            <font-awesome-icon :icon="`fa-brands fa-${item.brand}`" />
-          </a>
-        </span>
-        <a class="donor-link" role="button">
-          <span class="donor-button" aria-label="link to the donation page">
-            Become a donor
-          </span>
+        <a
+          class="mobile-menu-link"
+          v-for="item in socialMediaItems"
+          :key="item.brand"
+        >
+          {{ item.brand }}
         </a>
+        <a class="mobile-menu-link"> Become a Donor </a>
       </div>
     </div>
   </div>
@@ -74,14 +110,11 @@
 <script>
 export default {
   name: "HeaderBar",
-  mounted() {
-    this.setIsFocused();
-  },
   data() {
     return {
       currentPage: "Home",
-      isFocused: false,
-
+      accessibilityMenuOpen: false,
+      mobileMenuOpen: false,
       accessibilityMenu: [
         {
           label: "Dyslexia",
@@ -150,8 +183,13 @@ export default {
     setCurrentPage(page) {
       this.currentPage = page;
     },
-    setIsFocused() {
-      this.isFocused = !this.isFocused;
+    setAccessibilityMenuOpen() {
+      this.accessibilityMenuOpen = !this.accessibilityMenuOpen;
+    },
+    setMobileMenuOpen() {
+      window.visualViewport.width >= 375 && window.visualViewport.width <= 677
+        ? (this.mobileMenuOpen = !this.mobileMenuOpen)
+        : null;
     },
   },
 };
